840c0bdf3896662f447d6107dc6b57651635e857
yongnology <cassius20@naver.com>
2026-01-01 23:57:07 +0900

    최초 환경구성

---
+++ md_app/src/main/java/me/yongmin/md_app/MdAppApplication.java
@@ -0,0 +1,13 @@
+package me.yongmin.md_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MdAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MdAppApplication.class, args);
+	}
+
+}
---
+++ md_app/src/test/java/me/yongmin/md_app/MdAppApplicationTests.java
@@ -0,0 +1,13 @@
+package me.yongmin.md_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MdAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
